0eca5dd65b938517054e204802cc329232c679da
Jacob Allen <jedijake@me.com>
2016-06-29 14:17:58 -0700

    render images to page, render maps to page for each picture

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,55 @@
 console.log('JS loaded!');
 
-// var instaPicsTemplate = `
-//   <ul class="collection">
-//     <% if (instaPics.length > 0) { %>
-//       <% tasks.forEach(function(task) { %>
-//        <div class="row">
-//          <div class="col s12 m7">
-//            <div class="card">
-//              <div class="card-image">
-//                <img src="****IMAGE URL HERE!!!****">
-//                <span class="card-title">Card Title</span>
-//              </div>
-//              <div class="card-content">
-//                <p>****IMAGE TITLE HERE!!!****</p>
-//              </div>
-//            </div>
-//          </div>
-//        </div>
-//       <% }) %>
-//     <% } %>
-//   </ul>
-// `;
+var instaPicsTemplate = `
+  <ul class="collection">
+    <% if (likes.length > 0) { %>
+      <% likes.forEach(function(like, idx) { %>
+        <div class="row">
+          <div class="col s12 m7">
+            <div class="card">
+              <div class="card-image">
+                <img src="<%= like[0] %>">
+                <span class="card-title">Card Title</span>
+              </div>
+              <div class="card-content">
+                <div style="width: 600px; height: 600px" id="map-<%= idx %>">
+                  <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                  <% if (!like[1]) { %>
+                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                  <% } %>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <% }) %>
+    <%} %>
+  </ul>
+`;
+
+var renderInstaPics = _.template(instaPicsTemplate);
+
 function render() {
-  var instaPics = $.get('/api/likes', function(){
-    var pics      = instaPics.responseJSON;
-    var img       = $(`<img src=${pics[0][0]} />`);
-    $('body').append(img);
+  $.get('/api/likes', function(likes) {
+    console.log(likes)
+    $('#insta-pics').html(renderInstaPics({likes}));
+    likes.forEach(function(like, idx) {
+      if (like[1]) {
+        var map = new google.maps.Map(document.getElementById('map-' + idx), {
+          center: { lat: like[1].latitude,  lng: like[1].longitude },
+          zoom:   12
+        });
+        new google.maps.Marker({
+          position: { lat: like[1].latitude,  lng: like[1].longitude },
+          map: map,
+          title: like[1].name
+        });
+      }
+    })
   })
 };
 
 
-// var renderInstaPics = _.template(instaPicsTemplate);
 // function renderPage(uri, next) {
 //   $.ajax({
 //     method: 'GET',

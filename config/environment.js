0dca8eadf37b3ace0645774d2e8404d2940ae8d9
Adrian Delpha <adrian.delpha@gmail.com>
2016-06-30 05:22:15 -0700

    heroku deployment in process

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,8 @@ var _ = require('lodash');
 
 var localEnvVars = {
   TITLE:      'rnr',
-  SAFE_TITLE: 'rnr'
+  SAFE_TITLE: 'rnr',
+  MLAB_URI:   process.env.MLAB_URI
 };
 
 // Merge all environmental variables into one object.

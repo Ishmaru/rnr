61ad7f4a828d8c25962425df7abf7259c2cf040a
Adrian Delpha <adrian.delpha@gmail.com>
2016-06-30 05:06:18 -0700

    first attempt at heroku deploy

--- config/database.js
+++ config/database.js
@@ -1,21 +1,7 @@
 var mongoose = require('mongoose');
-
-var env = require('./environment');
-
-// Use different database URIs based on whether an env var exists.
-var dbUri = env.MLAB_URI ||
-            'mongodb://localhost/' + env.SAFE_TITLE;
-
-if (!env.MLAB_URI) {
-  // check that MongoD is running...
-  require('net').connect(27017, 'localhost').on('error', function() {
-    console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
-    process.exit(0);
-  });
-}
-
-if (!mongoose.connection._hasOpened) {
-  mongoose.connect(dbUri);
-}
-
+mongoose.connect(process.env.DATABASE_URL);
+// database connection event
+mongoose.connection.once('open', function (cb) {
+  console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
+});
 module.exports = mongoose;

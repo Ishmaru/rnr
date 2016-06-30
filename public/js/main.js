2af4365faddd9bf07f7d089b4d41df592ba63338
Jacob Allen <jedijake@me.com>
2016-06-30 10:44:26 -0700

    make tasty css

--- public/js/main.js
+++ public/js/main.js
@@ -4,15 +4,15 @@ var instaPicsTemplate = `
   <ul class="collection">
     <% if (likes.length > 0) { %>
       <% likes.forEach(function(like, idx) { %>
-        <div class="row">
-          <div class="col s12 m8 l8">
+        <div>
+          <div class="col s12 m6 l6">
             <div class="card">
               <div class="card-image">
                 <img src="<%= like[0] %>">
-                <span class="card-title">Card Title</span>
+                <span class="card-title"><%= like[1].name %></span>
               </div>
               <div class="card-content">
-                <div style="width: 250px; height: 250px" class="s9 m6 l3" id="map-<%= idx %>">
+                <div style="width: 300px; height: 250px" id="map-<%= idx %>">
                 </div>
               </div>
             </div>
@@ -22,36 +22,24 @@ var instaPicsTemplate = `
     <%} %>
   </ul>
 `;
-                  // <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                  // <% if (!like[1]) { %>
-                  //   <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                  // <% } %>
 
 // var instaPicsTemplate = `
-//   <ul class="collection">
 //     <% if (likes.length > 0) { %>
 //       <% likes.forEach(function(like, idx) { %>
-//         <div class="row">
-//           <div class="col s12 m6">
+//         <ul class="row">
+//           <li class="col l6 pull-l6">
 //             <div class="card">
-//               <div class="card-image waves-effect waves-block waves-light">
-//                 <img class="activator" src="<%= like[0] %>">
-//               </div>
-//               <div class="card-content">
-//                 <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-//               </div>
-//               <% if (like[1]) { %>
-//                 <div class="card-reveal">
-//                   <span class="card-title grey-text text-darken-4"><%= like[1].name %><i class="material-icons right">close</i></span>
-//                   <div style="width: 300px; height: 300px" id="map-<%= idx %>"></div>
-//                 </div>
-//               <% } %>
+//               <div class="card-image"><img src="<%= like[0] %>"></div>
 //             </div>
-//           </div>
-//         </div>
-//       <% }); %>
-//     <% } %>
-//   </ul>
+//           </li>
+//           <li class="col l6 push-l6">
+//             <div>
+//               <div style="width: 300px; height: 300px" class="right" id="map-<%= idx %>"></div>
+//             </div>
+//           </li>
+//           </ul>
+//       <% }) %>
+//     <%} %>
 // `;
 
 var renderInstaPics = _.template(instaPicsTemplate);
@@ -64,7 +52,7 @@ function render() {
       if (like[1]) {
         var map = new google.maps.Map(document.getElementById('map-' + idx), {
           center: { lat: like[1].latitude,  lng: like[1].longitude },
-          zoom:   12
+          zoom:   14
         });
         new google.maps.Marker({
           position: { lat: like[1].latitude,  lng: like[1].longitude },
